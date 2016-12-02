85fc2bbb7a2e20714f6805e9476b24492cc396ee
yaochenglong <289911401@qq.com>
2016-12-02 21:22:25 +0800

    添加测试程序

---
+++ multiple_thread/src/main/java/com/thread/Test.java
@@ -0,0 +1,10 @@
+package com.thread;
+
+/**
+ * Created by yaochenglong on 16/12/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试多线程");
+    }
+}
